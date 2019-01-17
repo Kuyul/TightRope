27902ebf0a638a9396afee7e7bed7e9a8b57c731
kyun1991 <38071517+kyun1991@users.noreply.github.com>
2019-01-17 21:06:19 +1300

    mid save

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Touch : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    private bool touching;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        touching = true;  
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        touching = false;
+    }
+}
